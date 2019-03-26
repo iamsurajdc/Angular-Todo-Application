63d503751f0f0583337215bc36a3ffdf86416bef
iamsurajdc <surajchandgude0304@gmail.com>
2019-03-26 18:09:38 +0530

    Added Todo module & todos component

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,38 @@
+import { Todo } from './../models/Todo';
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  constructor() { 
+    this.todos = [
+      {
+        id: 1,
+        title: 'Todo One',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: true,
+      },
+      {
+        id: 1,
+        title: 'Todo Three',
+        completed: false,
+      }
+
+
+    ]
+  }
+
+  ngOnInit() {
+  }
+
+}
